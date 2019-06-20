2abc8b93364592a46d96726a9fdade012bf8aa1a
CaH4aZzz <CaH4aZzz@ua.fm>
2019-06-20 10:43:55 +0300

    services added

--- src/main/java/com/maybe/maybe/service/OrderService.java
+++ src/main/java/com/maybe/maybe/service/OrderService.java
@@ -1,30 +1,30 @@
-//package com.maybe.maybe.service;
-//
-//import com.maybe.maybe.entity.Order;
-//import com.maybe.maybe.repository.OrderRepository;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class OrderService {
-//    private OrderRepository orderRepository;
-//
-//    public OrderService(OrderRepository orderRepository) {
-//        this.orderRepository = orderRepository;
-//    }
-//
-//
-//    public void save(Order order) {
-//        orderRepository.save(order);
-//    }
-//
-//    public List<Order> getAll() {
-//        return orderRepository.findAll();
-//    }
-//
-//    public Optional<Order> getById(Long id){
-//        return orderRepository.findById(id);
-//    }
-//}
+package com.maybe.maybe.service;
+
+import com.maybe.maybe.entity.Order;
+import com.maybe.maybe.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    private OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+
+    public void save(Order order) {
+        orderRepository.save(order);
+    }
+
+    public List<Order> getAll() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> getById(Long id){
+        return orderRepository.findById(id);
+    }
+}
