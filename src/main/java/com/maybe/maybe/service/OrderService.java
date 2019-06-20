6f69d0ccbc4c03092b4ccb7aff7cba4f43ad50b1
ArtemSobolenko <artem.sobolenko@gmail.com>
2019-06-20 11:42:08 +0300

    services added new

--- src/main/java/com/maybe/maybe/service/OrderService.java
+++ src/main/java/com/maybe/maybe/service/OrderService.java
@@ -1,7 +1,11 @@
 package com.maybe.maybe.service;
 
+import com.maybe.maybe.entity.Order;
 import com.maybe.maybe.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
@@ -9,5 +13,13 @@ public class OrderService {
 
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
+    }
+
+    public List<Order> getAll() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> getById(Long id){
+        return orderRepository.findById(id);
     }
 }
