1069def9dec74521456349459566c4ab8cb5f930
ArtemSobolenko <artem.sobolenko@gmail.com>
2019-06-20 12:46:13 +0300

    services added tests added

---
+++ src/test/java/com/maybe/maybe/service/OrderItemServiceTest.java
@@ -0,0 +1,16 @@
+package com.maybe.maybe.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderItemServiceTest {
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void getById() {
+    }
+}
